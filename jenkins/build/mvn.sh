a55c15fedb6b7dbd7fc8e373f9e78c7c9a6fb02f
Michael García <mgarcia@Michaels-MacBook-Pro-2.local>
2020-11-19 00:29:33 -0500

    test

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -8,8 +8,9 @@ WORKSPACE=/var/jenkins_home/workspace/pipeline-docker-maven
 echo $WORKSPACE
 echo $@
 
+ls -a $WORKSPACE/jenkins/maven/.m2/
+ls -a $WORKSPACE/java-app
 docker run --rm -v $WORKSPACE/java-app:/app -v $WORKSPACE/jenkins/maven/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
 # TESTS =================================================================
 # OK-Local: docker run --rm -v /Users/mgarcia/Documents/Jenkins/jenkins-data/pipeline/jenkins/maven/.m2/:/root/.m2/ -v /Users/mgarcia/Documents/Jenkins/jenkins-data/pipeline/java-app/:/app -w /app maven:3-alpine mvn -B -DskipTests clean package
-docker run --rm  -v  $WORKSPACE/java-app:/app -v $WORKSPACE/jenkins/maven/.m2/:/root/.m2/ -w /app maven:3-alpine mvn -B -DskipTests clean package
