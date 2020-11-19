66b31864e4627d9cd3c83d89c8edb915079134dc
Michael García <mgarcia@Michaels-MacBook-Pro-2.local>
2020-11-19 00:08:25 -0500

    Modify build/mvn.sh 5

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -9,6 +9,6 @@ echo $WORKSPACE
 echo $@
 
 # docker run --rm -v /Users/mgarcia/Documents/Jenkins/jenkins-data/pipeline/jenkins/maven/.m2/:/root/.m2/ -v /Users/mgarcia/Documents/Jenkins/jenkins-data/pipeline/java-app/:/app -w /app maven:3-alpine "$@"
-docker run --rm -v $WORKSPACE/jenkins/maven/.m2/:/root/.m2/ -v $WORKSPACE/java-app/:/app -w /app maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v $WORKSPACE/jenkins/maven/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
-# docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
