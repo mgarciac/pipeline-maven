4b5a64503663d83cd545ef794375aafdae1848d6
Michael García <mgarcia@Michaels-MacBook-Pro-2.local>
2020-11-18 22:33:59 -0500

    update mvn.sh in build and test folders

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,7 @@ echo '***************************************'
 echo '*********** Building JAR **************'
 echo '***************************************'
 
-docker run --rm -v /Users/mgarcia/Documents/Jenkins/jenkins-data/pipeline/jenkins/maven/.m2/:/root/.m2/ -v /Users/mgarcia/Documents/Jenkins/jenkins-data/pipeline/java-app/:/app -w /app maven:3-alpine "$@"
+WORKSPACE=/var/jenkins_home/workspace/pipeline-docker-maven
+
+# docker run --rm -v /Users/mgarcia/Documents/Jenkins/jenkins-data/pipeline/jenkins/maven/.m2/:/root/.m2/ -v /Users/mgarcia/Documents/Jenkins/jenkins-data/pipeline/java-app/:/app -w /app maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/jenkins/maven/.m2/:/root/.m2/ -v $WORKSPACE/jenkins/java-app/:/app -w /app maven:3-alpine "$@"
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,4 +4,7 @@ echo '***************************************'
 echo '******** Testing the code  ************'
 echo '***************************************'
 
-docker run --rm -v /Users/mgarcia/Documents/Jenkins/jenkins-data/pipeline/jenkins/maven/.m2/:/root/.m2/ -v /Users/mgarcia/Documents/Jenkins/jenkins-data/pipeline/java-app/:/app -w /app maven:3-alpine "$@"
+WORKSPACE=/var/jenkins_home/workspace/pipeline-docker-maven
+
+# docker run --rm -v /Users/mgarcia/Documents/Jenkins/jenkins-data/pipeline/jenkins/maven/.m2/:/root/.m2/ -v /Users/mgarcia/Documents/Jenkins/jenkins-data/pipeline/java-app/:/app -w /app maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/jenkins/maven/.m2/:/root/.m2/ -v $WORKSPACE/java-app/:/app -w /app maven:3-alpine "$@"
