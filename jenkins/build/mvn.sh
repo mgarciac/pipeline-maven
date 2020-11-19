a9682644adb1325603e0c0b20c40d37b04db1816
Michael García <mgarcia@Michaels-MacBook-Pro-2.local>
2020-11-19 11:16:12 -0500

    Test 5

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -14,7 +14,8 @@ echo ' '
 echo 'Directory: $WORKSPACE/java-app'
 ls -l $WORKSPACE/java-app
 
-docker run --rm -v $WORKSPACE/java-app:/app -v $WORKSPACE/jenkins/maven/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/jenkins/maven/.m2/:/root/.m2/ -v $WORKSPACE/java-app/:/app -w /app maven:3-alpine mvn -B -DskipTests clean package "$@"
 
 # TESTS =================================================================
 # OK-Local: docker run --rm -v /Users/mgarcia/Documents/Jenkins/jenkins-data/pipeline/jenkins/maven/.m2/:/root/.m2/ -v /Users/mgarcia/Documents/Jenkins/jenkins-data/pipeline/java-app/:/app -w /app maven:3-alpine mvn -B -DskipTests clean package
+# docker run --rm -v $WORKSPACE/jenkins/maven/.m2/:/root/.m2/ -v $WORKSPACE/java-app/:/app -w /app maven:3-alpine mvn -B -DskipTests clean package
